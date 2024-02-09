7af9d41b9d03be4d35912f586aafcb63e3a7a188
NatNichols <natalie.e.nichols@gmail.com>
2024-02-09 17:46:30 -0500

    implement create PantryItem functionality

--- routes/pantryItems.js
+++ routes/pantryItems.js
@@ -14,7 +14,7 @@ router.use(decodeUserFromToken)
 // GET to localhost:3001/api/pantryItems/:pantryItemId (show)
 
 // POST to localhost:3001/api/pantryItems (create)
-
+router.post('/', checkAuth, pantryItemsCtrl.create)
 // PUT to localhost:3001/api/pantryItems/:pantryItemId (update)
 
 // DELETE to localhost:3001/api/pantryItems/:pantryItemId (delete)
