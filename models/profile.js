441ffb7561525f555057e94121f6ff2c2388dc86
NatNichols <natalie.e.nichols@gmail.com>
2024-02-09 18:13:17 -0500

    add pantryItems array to Profile model

--- models/profile.js
+++ models/profile.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema
 
 const profileSchema = new Schema({
   name: String,
-  photo: String
+  photo: String,
+  pantryItems: [{ type: Schema.Types.ObjectId, ref: 'PantryItem' }],
 },{
   timestamps: true,
 })
