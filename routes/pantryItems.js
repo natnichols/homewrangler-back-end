65d4670648d23f3b87cc2dfc7eead29aabaa7c22
NatNichols <natalie.e.nichols@gmail.com>
2024-02-09 16:58:54 -0500

    stub up routes/pantryItems.js

---
+++ routes/pantryItems.js
@@ -0,0 +1,25 @@
+import { Router } from 'express'
+import * as pantryItemsCtrl from '../controllers/pantryItems.js'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
+
+const router = Router()
+
+// ========== Public Routes ===========
+
+
+// ========= Protected Routes ========= 
+router.use(decodeUserFromToken)
+// GET to localhost:3001/api/pantryItems (index)
+
+// GET to localhost:3001/api/pantryItems/:pantryItemId (show)
+
+// POST to localhost:3001/api/pantryItems (create)
+
+// PUT to localhost:3001/api/pantryItems/:pantryItemId (update)
+
+// DELETE to localhost:3001/api/pantryItems/:pantryItemId (delete)
+
+
+export {
+  router
+}
