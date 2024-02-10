7c06d8895a05621c3a6ab470d3df55c7f21fa9a5
NatNichols <natalie.e.nichols@gmail.com>
2024-02-09 20:27:44 -0500

    stub up routes/repairs.js, write POST route for create ctrl

---
+++ routes/repairs.js
@@ -0,0 +1,23 @@
+import { Router } from 'express'
+import * as repairsCtrl from '../controllers/repairs.js'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth'
+
+const router = Router()
+
+// ========== Public Routes ===========
+
+
+// ========= Protected Routes ========= 
+router.use(decodeUserFromToken)
+// GET to localhost:3001/api/repairs
+
+// GET to localhost:3001/api/repairs/:repairId
+
+// POST to localhost:3001/api/repairs
+router.post('/', checkAuth, repairsCtrl.create)
+// PUT to localhost:3001/api/repairs/:repairId
+
+// DELETE to localhost:3001/api/repairs/:repairId
+
+
+export { router }
