475c201c4c53c814c9b5ce3509c863a152071e79
NatNichols <natalie.e.nichols@gmail.com>
2024-02-09 20:15:49 -0500

    stub up routes/budgets.js, write POST route for create ctrl

---
+++ routes/budgets.js
@@ -0,0 +1,23 @@
+import { Router } from 'express'
+import * as budgetsCtrl from '../controllers/budgets.js'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
+
+const router = Router()
+
+// ========== Public Routes ===========
+
+
+// ========= Protected Routes ========= 
+router.use(decodeUserFromToken)
+// GET to localhost:3001/api/budgets
+
+// GET to localhost:3001/api/budgets/:budgetId
+
+// POST to localhost:3001/api/budgets
+router.post('/', checkAuth, budgetsCtrl.create)
+// PUT to localhost:3001/api/budgets/:budgetId
+
+// DELETE to localhost:3001/api/budgets/:budgetId
+
+
+export { router }
