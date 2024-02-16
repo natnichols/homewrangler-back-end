6f3a6777c2cc4dc29dd90f628f88f3970e54ef5e
NatNichols <natalie.e.nichols@gmail.com>
2024-02-16 16:47:53 -0500

    fix import quotes

--- controllers/budgets.js
+++ controllers/budgets.js
@@ -1,5 +1,5 @@
-import { Budget } from "../models/budget.js"
-import { Profile } from "../models/profile.js"
+import { Budget } from '../models/budget.js'
+import { Profile } from '../models/profile.js'
 
 export async function create(req, res) {
   try {
